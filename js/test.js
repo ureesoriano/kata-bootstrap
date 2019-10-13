cfc03d931974d1b92b7ef79e140b8bcd5874d8b9
Oriol Soriano <oriolsoriano@gmail.com>
2019-10-13 17:26:34 +0200

    Add kata bootstrap for javascript

---
+++ js/test.js
@@ -0,0 +1,37 @@
+const should = require('chai').should();
+const _ = require('lodash');
+
+const XXX = require('./XXX');
+
+describe.only('Setup', () => {
+  it('is OK', () => {
+    XXX().should.equal(1);
+  });
+});
+
+describe.skip(`XXX function`, () => {
+  beforeEach(() => {
+    // ...
+  });
+
+  it(`it should ..`, () => {
+    contains_array(XXX(), [50,50,50,50,50]).should.be.true;
+  });
+
+  // ...
+});
+
+describe(`contains_array function`, () => {
+  it('returns true with a contained array', () => {
+    expect(contains_array([[1,2,3],[1,2],[1]], [1])).to.be.true;
+  });
+
+  // ...
+});
+
+const contains_array = (arrays, array) => {
+  return arrays
+    .filter(a =>
+      _.isEqual(a, array)
+    ).length > 0;
+}
